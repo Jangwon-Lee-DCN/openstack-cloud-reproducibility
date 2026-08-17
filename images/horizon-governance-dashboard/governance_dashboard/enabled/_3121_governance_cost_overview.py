4830a6fdb18cb51b6d3530dd17245fc55902093d
Jangwon Lee <44286444+Jangwon-Lee-DCN@users.noreply.github.com>
2026-08-17 15:50:36 +0000

    fix(horizon): keep cost management under governance

--- images/horizon-governance-dashboard/governance_dashboard/enabled/_3121_governance_cost_overview.py
+++ images/horizon-governance-dashboard/governance_dashboard/enabled/_3121_governance_cost_overview.py
@@ -1,4 +1,4 @@
 PANEL = "cost_overview"
-PANEL_DASHBOARD = "project"
+PANEL_DASHBOARD = "governance"
 PANEL_GROUP = "cost_management"
 ADD_PANEL = "governance_dashboard.panels.cost_overview.panel.CostOverview"
--- images/horizon-governance-dashboard/governance_dashboard/enabled/_3123_governance_aws_forecast.py
+++ images/horizon-governance-dashboard/governance_dashboard/enabled/_3123_governance_aws_forecast.py
@@ -1,4 +1,4 @@
 PANEL = "aws_cost_forecast"
-PANEL_DASHBOARD = "project"
+PANEL_DASHBOARD = "governance"
 PANEL_GROUP = "cost_management"
 ADD_PANEL = "governance_dashboard.panels.aws_forecast.panel.AwsCostForecast"
